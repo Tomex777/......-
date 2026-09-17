b309cc925005b8d5a24412b7f98be29ad2497ae5
Tomex777 <patrickcoker737@gmail.com>
2026-09-17 11:04:48 +0100

    refactor: route view-once opening through capability hub

--- commands/viewonce.js
+++ commands/viewonce.js
@@ -1,9 +1,7 @@
-import { downloadQuotedMedia } from '../src/features/media.js';
-
 export default {
   name:'viewonce',aliases:['vv'],ownerOnly:true,requiresAllowedChat:true,requiresAI:false,feature:'media',
-  async execute({raw,reply}){
-    const media=await downloadQuotedMedia(raw);
+  async execute({raw,reply,features}){
+    const media=await features.openViewOnce(raw);
     if(media.kind==='image') await reply({image:media.buffer,caption:'View once opened'});
     else if(media.kind==='video') await reply({video:media.buffer,caption:'View once opened'});
     else if(media.kind==='audio') await reply({audio:media.buffer,mimetype:media.mimetype||'audio/ogg'});
